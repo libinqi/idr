762ac783177f0ac61e5fa9857eb90f17769d6a41
libinqi <libinqi@outlook.com>
2016-11-25 15:38:48 +0800

    修改配置文件查找问题

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ var fs = require('fs');
 var pkg = require(path.join(process.cwd(), 'package.json'));
 
 var idr = null;
+var config = null;
 var configPath = path.join(process.cwd(), 'device.json');
 if (pkg.idr && pkg.idr.driver) {
     idr = require(pkg.idr.driver);
 } else if (fs.existsSync(configPath)) {
-    var config = JSON.parse(fs.readFileSync(configPath));
+    config = JSON.parse(fs.readFileSync(configPath));
     if (config.idr && config.idr.driver) {
         idr = require(config.idr.driver);
     }
 } else {
     configPath = path.join(process.cwd(), 'config.json');
-    var config = JSON.parse(fs.readFileSync(configPath));
-    if (config.idr && config.idr.driver) {
-        idr = require(config.idr.driver);
-    }
+	if (fs.existsSync(configPath)) {
+		config = JSON.parse(fs.readFileSync(configPath));
+		if (config.idr && config.idr.driver) {
+			idr = require(config.idr.driver);
+		}
+	}
 }
 
 module.exports = idr;
